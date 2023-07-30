e4d5892e62db1c292364e42058d0af722868e07e
Bilal-Ahmed1999 <bnagouri55@gmail.com>
2023-07-30 23:46:38 +0530

    second commit hai

--- src/components/DarkmodeToggle/DarkmodeToggle.tsx
+++ src/components/DarkmodeToggle/DarkmodeToggle.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useContext } from "react";
-import styles from "./darkModeToggle.module.css";
+import styles from './DarkmodeToggle.module.css'
 import { useTheme } from 'next-themes'
 
 
